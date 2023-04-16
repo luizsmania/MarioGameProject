73cb8d9a27cb3b3474bcb720ce90e77171e99a76
Luiz Smania <luizsmania1@gmail.com>
2023-04-16 13:31:41 +0000

    many changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,12 @@ const clouds = document.querySelector('.clouds');
 const restartButton = document.querySelector('.gameover');
 const gameItself = document.querySelector('.gameitself')
 const startGameButton = document.querySelector('.startButton')
-let skin = 'mario'
-
-// document.addEventListener('click', startGame())
-
-
-
+let skin = 'mario';
 
 function startGame() {
-    const playBtn = document.querySelector('.playbtn')
+    const playBtn = document.querySelector('.playbtn');
     const gameOver = document.querySelector('.gameover');
     const tryAgain = document.querySelector('.tryagain');
-    let isGameOver = false;
-    // clouds.style.animation = 'clouds 18s infinite linear';
-    
-    pipe.style.animation = 'pipe-animation 1.5s infinite linear';
-    // mario.style.animation = 'none';
-    // mario.style.bottom = `${marioPosition}px`;
 
     const jump = () => {
         mario.classList.add('jump');
@@ -33,15 +22,13 @@ function startGame() {
     document.addEventListener('keydown', jump);
     gameBoard.addEventListener('mousedown', jump);
     
+
     gameItself.style.visibility = 'visible';
     playBtn.style.visibility = 'hidden'
     startGameButton.style.visibility = 'hidden';
-    pipe.style.visibility = 'hidden';
+    pipe.style.visibility = 'visible';
     gameOver.style.visibility = 'hidden';
     tryAgain.style.visibility = 'hidden';
-    mario.style.animation = '';
-    mario.style.width = '150px';
-    pipe.style.animation = '';
     clouds.style.animation = '';
     pipe.style.left = '';
     pipe.style.right = '';
@@ -50,10 +37,19 @@ function startGame() {
         mario.src = 'assets/images/mario.webp';
         clouds.src = 'assets/images/clouds.png';
         gameBoard.style.backgroundImage = 'linear-gradient(#87CEEB, #E0F6FF)'
-        mario.style.width = '150px'
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            mario.style.width = '80px'
+          } else {
+            mario.style.width = '150px'
+          };
+
         mario.style.bottom = '0px'
         pipe.src = 'assets/images/pipe.png';
-        pipe.style.width = '170px';
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            pipe.style.width = '100px'
+          } else {
+            pipe.style.width = '170px'
+          };
         mario.style.bottom = '-15px'
     }else if(skin === 'pikachu'){
         mario.src = 'assets/images/pikachu.webp';
@@ -94,6 +90,7 @@ function startGame() {
         pipe.style.width = '100px';
         pipe.style.marginBottom = '-35px'
     }
+    mario.style.animation = '';
     pipe.style.animation = 'pipe-animation 1.5s infinite linear';
 
     let scoreLoop = setInterval(() => {
@@ -102,59 +99,30 @@ function startGame() {
         const newScore = currentScore + 15;
         scoreSpan.textContent = newScore.toString();
     }, 500);
-    
+
     let coinsLoop = setInterval(() => {
         const coinsSpan = document.querySelector('.coins');
         const currentCoins = parseInt(coinsSpan.textContent);
-        const newCoins = currentCoins + 2;
+        const newCoins = currentCoins + 200;
         coinsSpan.textContent = newCoins.toString();
     }, 800);
 
-    // const pipeAnimationInterval = setInterval(() => {
-    //     const pipe = document.querySelector('.pipe');
-    //     const pipeAnimationTime = parseFloat(getComputedStyle(pipe).animationDuration.replace('s', ''));
-    //     const speedSpan = document.querySelector('.speed');
-    //     const speedCounter = document.querySelector('.speedCount')
-      
-    //     console.log('Pipe computed style:', getComputedStyle(pipe));
-      
-    //     if (pipeAnimationTime > 0.7) {
-    //       speedSpan.textContent = parseInt(speedSpan.textContent) + 1;
-    //       speedCounter.style.visibility = 'visible';
-    //       pipe.style.visibility = 'hidden';
-    //       pipe.style.right = '-500px';
-    //       pipe.style.animation = 'none';
-          
-    //       setTimeout(() => {
-    //         pipe.style.right = '';
-    //         pipe.style.visibility = 'visible';
-    //         pipe.style.animation = `pipe-animation ${pipeAnimationTime - 0.1}s infinite linear`;
-    //         speedCounter.style.visibility = 'hidden';
-            
-    //       }, 2000);
-    
-    //     }
-    //   }, 12000);
+   const loop = setInterval(() => {
 
-    setTimeout(() => {
-    let loop = setInterval(() => {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
         const cloudPosition = +window.getComputedStyle(clouds).right.replace('px', '');
-        
-
-        pipe.style.visibility = 'visible'
 
         
 
-        if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80 && !isGameOver) {
-            isGameOver = true;
-            if(isGameOver){
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePosition}px`;
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
-
+            clouds.style.animation = 'none';
+            clouds.style.right = `${cloudPosition}px`;
             if (skin === 'mario') {
                 mario.src = 'assets/images/gameover.png'
                 mario.style.width = '75px';
@@ -162,6 +130,7 @@ function startGame() {
             } else if (skin === 'pikachu') {
                 mario.src = 'assets/images/pikaover.png'
                 mario.style.width = '130px';
+                mario.style.marginLeft = '50px';
                 mario.style.bottom = `${marioPosition}px`;
             } else if (skin === 'sonic') {
                 mario.src = 'assets/images/sonicover.png';
@@ -171,34 +140,24 @@ function startGame() {
                 mario.src = 'assets/images/misteryover.png';
 
             }
-
-            clouds.style.animation = 'none';
-            clouds.style.right = `${cloudPosition}px`;
-            clouds.style.clear = 'both';
-
             gameOver.style.visibility = 'visible';
             tryAgain.style.visibility = 'visible';
-
             const scoreSpan = document.querySelector('.score');
             const currentScore = parseInt(scoreSpan.textContent);
             const bestScore = currentScore;
             const bestScoreSpan = document.querySelector('.bestScore');
-            if (bestScoreSpan.textContent < currentScore){
+            if (bestScoreSpan.textContent < currentScore) {
                 bestScoreSpan.textContent = bestScore.toString();
             }
-            
-            scoreSpan.textContent = '0';
-            const speedSpan = document.querySelector('.speed');
-            speedSpan.textContent = '1';
             clearInterval(loop);
             clearInterval(scoreLoop);
             clearInterval(coinsLoop);
-            clearInterval(increaseCoinsLoop);
-            clearInterval(pipeAnimationInterval);
-        }}
-    }, 100);}, 1);}
+            scoreSpan.textContent = '0';
+        }
+    }, 10);
+}
 
-    
+
 
 
 function restart() {
@@ -341,4 +300,3 @@ function skinMistery() {
         }, 3000)
     }
 }
-
